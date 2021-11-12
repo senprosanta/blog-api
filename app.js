33c4d21a9773904906d969ccd60d30f70dc239b2
senprosanta <devoloper.sen@gmail.com>
2021-11-12 16:45:48 +0530

    environment modified

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
 
 //Import router
 const router = require('./routes/routes')
@@ -11,6 +12,17 @@ const app = express()
 
 //server static files
 app.use(express.static('public'))
+
+//Enable cors
+app.use(cors())
+
+//morgan
+if (process.env.NODE_ENV === process.env.DEV) {
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+}
+
+//helmet
+app.use(helmet())
 
 //set routes
 app.use('/api/v1', router)
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const app = require('./app')
 
+require('dotenv').config()
+
 const port = process.env.PORT || 5050
+
 
 
 //Start server
 app.listen(port, () => {
     console.log(`server is running on port ${port}...`);
+    console.log(`Node environment is ${process.env.NODE_ENV}`);
 })
